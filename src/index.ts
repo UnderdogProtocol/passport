6436782aeb05f27636a396886c1b142df6d31690
Kevin Arifin <kevarifin14@gmail.com>
2023-11-21 08:05:42 -0800

    add function to create transaction to activate user passport with a domain

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import { createUmi } from "@metaplex-foundation/umi";
+import {
+  createNoopSigner,
+  createUmi,
+  publicKey,
+} from "@metaplex-foundation/umi";
 import { web3JsEddsa } from "@metaplex-foundation/umi-eddsa-web3js";
 import { defaultProgramRepository } from "@metaplex-foundation/umi-program-repository";
+import { PublicKey } from "@solana/web3.js";
 import {
+  activatePassportV0,
   createUnderdogIdentityProgram,
   findLinkPda,
 } from "@underdog-protocol/underdog-identity-sdk";
+
+import { toWeb3JsTransaction } from "@metaplex-foundation/umi-web3js-adapters";
 
 const context = createUmi();
 
@@ -18,12 +26,34 @@ type SeedsInput = {
   identifier: string;
 };
 
-export const getPassportAddress = (seeds: SeedsInput) => {
-  const namespace =
+const getNamespaceFromSeeds = (seeds: SeedsInput) => {
+  return (
     seeds.namespace ||
     process.env.NAMESPACE ||
     process.env.NEXT_PUBLIC_NAMESPACE ||
-    "public";
+    "public"
+  );
+};
+
+export const getPassportAddress = (seeds: SeedsInput) => {
+  const namespace = getNamespaceFromSeeds(seeds);
 
   return findLinkPda(context, { ...seeds, namespace })[0];
+};
+
+export const activatePassport = (
+  seeds: SeedsInput,
+  domainAuthority: PublicKey,
+  passportAuthority: PublicKey
+) => {
+  const namespace = getNamespaceFromSeeds(seeds);
+
+  return toWeb3JsTransaction(
+    activatePassportV0(context, {
+      ...seeds,
+      namespace,
+      domainAuthority: createNoopSigner(publicKey(domainAuthority)),
+      passportAuthority: createNoopSigner(publicKey(passportAuthority)),
+    }).build(context)
+  );
 };
